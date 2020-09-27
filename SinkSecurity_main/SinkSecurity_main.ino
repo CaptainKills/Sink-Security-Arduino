78fa05788a2daabebd39329bcce3069a7073c40e
CaptainKills <danick@triantis.nl>
2020-09-27 09:59:05 +0200

    Split Code into main and companion

---
+++ SinkSecurity_main/SinkSecurity_main.ino
@@ -0,0 +1,34 @@
+//Project Libraries
+#include <RH_ASK.h>
+#include <SPI.h>
+
+//PIN Constants
+#define WATER_PIN A1
+
+//Variable Objects
+RH_ASK driver;
+
+void setup() {
+  //Serial Debugging Setup
+  Serial.begin(9600);
+
+  //Driver initialisation
+  if(!driver.init()){
+    Serial.println("Driver initialisation failed!");
+  }
+
+  //PinMode initialisation
+  pinMode(INPUT, WATER_PIN);
+}
+
+void loop() {
+  int input = analogRead(WATER_PIN);
+  Serial.println(input);
+
+  
+}
+
+void sendMessage(char *msg){
+  driver.send((uint8_t *)msg, strlen(msg));
+  driver.waitPacketSent(); 
+}
