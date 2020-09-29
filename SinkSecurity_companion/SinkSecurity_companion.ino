c70802abc6c9c077eaa704754311f1b8a7837546
CaptainKills <danick@triantis.nl>
2020-09-29 11:52:24 +0200

    Added Message checking
    Message is now checked for Level constants. From here we can add motor control.

--- SinkSecurity_companion/SinkSecurity_companion.ino
+++ SinkSecurity_companion/SinkSecurity_companion.ino
@@ -3,10 +3,15 @@
 #include <SPI.h>  //Support library for TX/RX library
 
 //Constants
-#define MESSAGE_LENGTH 12
+#define MESSAGE_LENGTH 7
+#define LEVEL_0 "LEVEL_0"
+#define LEVEL_1 "LEVEL_1"
+#define LEVEL_2 "LEVEL_2"
+#define LEVEL_3 "LEVEL_3"
 
 //Variable Objects
 RH_ASK driver;
+char *message;
 
 void setup() {
   //Serial Debugging Setup
@@ -23,12 +28,28 @@ void setup() {
 
 void loop() {
   //Create space for message in memory
-  char *message = (char *) malloc(MESSAGE_LENGTH);
+  message = (char *) malloc(MESSAGE_LENGTH);
   //Recieve the message
   message = recieveMessage();
   
   Serial.print("Message: ");
   Serial.println(message);
+
+  //Check which message has been sent
+  if(message == LEVEL_1){ //Level 1: light vibrations.
+    Serial.println("Level 1 Detected: Set Vribration Level to ...");
+    
+  } else if(message == LEVEL_2){ //Level 2: medium vibrations.
+    Serial.println("Level 2 Detected: Set Vribration Level to ...");
+    
+  } else if(message == LEVEL_3){ //Level 3: strong vibrations.
+    Serial.println("Level 3 Detected: Set Vribration Level to ...");
+   
+  } else if(message == LEVEL_0){ //Level 0: disable vibrations.
+    Serial.println("Level 0 Detected: Turning off.");
+  } else{
+    //Default case: no message revieved.
+  }
 
   //Free space in memory
   free(message);
