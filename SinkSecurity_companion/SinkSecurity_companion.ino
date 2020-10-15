f94a7d615945c9d41b94bfcc09c6da62368c8ced
Danick Triantis <danick@triantis.nl>
2020-10-15 13:27:38 +0200

    Removed unused debugging Serial
    ATTiny85 does not have Serial, so debugging statements are useless.

--- SinkSecurity_companion/SinkSecurity_companion.ino
+++ SinkSecurity_companion/SinkSecurity_companion.ino
@@ -5,7 +5,7 @@
 #endif
 
 //Constants
-#define MOTOR_PIN 6
+#define MOTOR_PIN 5
 #define BUF_LENGTH 6
 #define LEVEL_0 "LEVEL_0"
 #define LEVEL_1 "LEVEL_1"
@@ -17,15 +17,16 @@ RH_ASK driver(2000, 2, 3);
 
 void setup() {
   //Serial Debugging Setup
-  Serial.begin(9600);
+  //Serial.begin(9600);
 
   //Driver initialisation
-  if(!driver.init()){
-    Serial.println("Driver initialisation failed!");
-  }
+  //if(!driver.init()){
+    //Serial.println("Driver initialisation failed!");
+  //}
 
   //PinMode initialisation
-  pinMode(OUTPUT, A0);
+  pinMode(MOTOR_PIN, OUTPUT);
+  setMotorLevel(1);
 }
 
 void loop() {
@@ -33,30 +34,31 @@ void loop() {
   char *message = "";
   recieveMessage(&message);
   
-  Serial.print("Message: ");
-  Serial.println(message);
-  delay(500);
+  //Serial.print("Message: ");
+  //Serial.println(message);
 
   //Check which message has been sent
-  if(strcmp(message, LEVEL_1) == 0){ //Level 1: light vibrations.
-    Serial.println("Level 1 Detected: Set Vribration Level to 1");
+  if(strcmp(message, LEVEL_1) == 0){ //Level 1: light vibrations.`
+    //Serial.println("Level 1 Detected: Set Vribration Level to 1");
     setMotorLevel(1);
     
   } else if(strcmp(message, LEVEL_2) == 0){ //Level 2: medium vibrations.
-    Serial.println("Level 2 Detected: Set Vribration Level to 2");
-    setMotorLevel(2);
+    //Serial.println("Level 2 Detected: Set Vribration Level to 2");
+    setMotorLevel(2); 
     
-  } else if(strcmp(message, LEVEL_3) == 0){ //Level 3: strong vibrations.
-    Serial.println("Level 3 Detected: Set Vribration Level to 3");
+  } else if(strcmp(message, LEVEL_3) == 0){ //c 3: strong vibrations.
+    //Serial.println("Level 3 Detected: Set Vribration Level to 3");
     setMotorLevel(3);
    
   } else if(strcmp(message, LEVEL_0) == 0){ //Level 0: disable vibrations.
-    Serial.println("Level 0 Detected: Turning off.");
+    //Serial.println("Level 0 Detected: Turning off.");
     setMotorLevel(0);
     
   } else{
     //Default case: no message received.
   }
+
+  delay(100);
 }
 
 void setMotorLevel(int level){
@@ -64,7 +66,7 @@ void setMotorLevel(int level){
   if(level != 0){
     pwm = 105 + (50 * level);
   }
-  analogWrite(MOTOR_PIN, pwm);
+  digitalWrite(MOTOR_PIN, pwm);
 }
 
 void recieveMessage(char **msg){
